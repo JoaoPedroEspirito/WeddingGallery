2def4de6f6de8c8710dac4d756d8e9746d62780e
JoaoPedroEspirito <jparaujo2904@gmail.com>
2026-08-31 08:14:45 -0300

    alterando conexao do front para deploy

--- WeddingGallery/frontend/src/services/api.js
+++ WeddingGallery/frontend/src/services/api.js
@@ -1,5 +1,5 @@
 ﻿import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://localhost:7189/api',
+    baseURL: import.meta.env.VITE_API_URL || 'https://localhost:7189/api'
 });
